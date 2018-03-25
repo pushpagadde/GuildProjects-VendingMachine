7a94828acbe114115af41acc40088913a5891ae0
pushpagadde <pushpa_gadde@yahoo.com>
2018-03-24 21:25:15 -0400

    'sql'

--- SuperHeroSighting/SuperHeroSighting.sql
+++ SuperHeroSighting/SuperHeroSighting.sql
@@ -1,11 +1,18 @@
 drop database if exists SuperHeroSighting;
 create database superHerosighting;
 
-CREATE TABLE SuperHeroSighting.ZipCodeInfo
-(ZipCode VARCHAR(5) NOT NULL,
-City VARCHAR(20) NOT NULL,
-State VARCHAR(2) NOT NULL,
-PRIMARY KEY (ZipCode)
+
+CREATE TABLE SuperHeroSighting.authorities
+(username varchar(20) NOTNULL,
+authority varchar(20) NOTNULL,
+FOREIGN KEY (authority) REFERENCES USERS(AUTHORITY)
+);
+
+CREATE TABLE  superHerosighting.Hero_Organization (
+HeroID int NOT NULL,
+OrganizationID int NOT NULL,
+foreign key (HeroID) references SuperHero(HeroID),
+foreign key (OrganizationID) references Organizations(OrganizationID)
 );
 
 CREATE TABLE  superHerosighting.Location (
@@ -19,13 +26,6 @@ CREATE TABLE  superHerosighting.Location (
  FOREIGN KEY (ZipCode) REFERENCES ZipCodeInfo(ZipCode)
 );
 
-CREATE TABLE superHerosighting.SuperHero (
-HeroID int NOT NULL auto_increment,
-HeroName varchar(20) NOT NULL,
-HeroPower varchar(20) NOT NULL,
-PRIMARY KEY (HeroID)
-);
-
 CREATE TABLE superHerosighting.Members (
 MemberID int NOT NULL auto_increment,
 FirstName varchar(20),
@@ -34,6 +34,13 @@ Address varchar(20),
 ZipCode varchar(5),
 primary key (MemberID),
 foreign key (ZipCode) references ZipCodeInfo(ZipCode)
+);
+
+CREATE TABLE superHerosighting.OrganizationMember (
+OrganizationID INT NOT NULL auto_increment,
+MemberID int NOT NULL,
+foreign key (OrganizationID) references Organizations(OrganizationID),
+foreign key (MemberID) references Members(MemberID)
 );
 
 CREATE TABLE superHerosighting.Organizations (
@@ -46,26 +53,35 @@ primary key (organizationID),
 foreign key (ZipCode) references ZipCodeInfo(ZipCode)
 );
 
-CREATE TABLE  superHerosighting.Hero_Organization (
-HeroID int NOT NULL,
-OrganizationID int NOT NULL,
-foreign key (HeroID) references SuperHero(HeroID),
-foreign key (OrganizationID) references Organizations(OrganizationID)
-);
-
-CREATE TABLE superHerosighting.OrganizationMember (
-OrganizationID INT NOT NULL auto_increment,
-MemberID int NOT NULL,
-foreign key (OrganizationID) references Organizations(OrganizationID),
-foreign key (MemberID) references Members(MemberID)
-);
-
-
 CREATE TABLE superHerosighting.Sightings (
-HeroId int NOT NULL auto_increment,
+sightingID NOT NULL  auto_increment, 
+HeroId int NOT NULL,
 LocationID int NOT NULL,
 DateOfSighting DateTime NOT NULL,
+Primary key (sightingID),
 FOREIGN KEY (HeroID) REFERENCES SuperHero(HeroID),
 FOREIGN KEY (LocationID) REFERENCES Location(LocationID) 
+);
+
+CREATE TABLE superHerosighting.SuperHero (
+HeroID int NOT NULL auto_increment,
+HeroName varchar(20) NOT NULL,
+HeroPower varchar(20) NOT NULL,
+PRIMARY KEY (HeroID)
+);
+
+CREATE TABLE SuperHeroSighting.users
+(user_id int(11),
+username varchar(20),
+password varchar(100),
+Enabled tinyint(1)
+PRIMARY KEY('USER_ID')
+);
+
+CREATE TABLE SuperHeroSighting.ZipCodeInfo
+(ZipCode VARCHAR(5) NOT NULL,
+City VARCHAR(20) NOT NULL,
+State VARCHAR(2) NOT NULL,
+PRIMARY KEY (ZipCode)
 );
 
